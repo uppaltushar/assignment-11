22feae722540eb2ea7c220f698e82c9a5f9e8887
uppaltushar <40592029+uppaltushar@users.noreply.github.com>
2018-08-30 18:30:23 +0530

    Add files via upload

--- regularexpression-1.py
+++ regularexpression-1.py
@@ -6,14 +6,11 @@ for i in b:
     match=re.finditer('^[\w]*(@)(gmail.com|yahoo.com)',i)
     for i in match:
         print(i.group())
+        
 
 
 #2
 import re
-a=('the text is +918894897895')
-b=list(a.split(' '))
-
-for i in b:
-    match=re.finditer('^[+91][6-9][0-9]{11}',i)
-    for i in match:
+match=re.finditer('[+]91-[6-9][0-9]{9}','Mobile Number: +91-8894897895, +91-7986473580 , 3448 ,+91-9459023322')
+for i in match:
         print(i.group())
